75d261a4e98a81bc0abf5c78a1a48c06ad445c5e
zuccha <amedeo.zucchetti@usi.ch>
2016-05-01 22:37:12 +0200

    Update notes_converter.cpp

--- notes_converter.cpp
+++ notes_converter.cpp
@@ -16,64 +16,68 @@ const int EXAMPLE = 2;
 
 void generate_tex(const std::string &file, bool proofs, bool examples, bool def)
 {
-	std::ofstream tex_proofs("calculus_notes_proofs.tex");
-	std::ofstream tex_examples("calculus_notes_examples.tex");
-	std::ofstream tex_def("calculus_notes_definitions.tex");
-	std::ifstream tex_source(file);
+  std::ofstream tex_proofs("calculus_notes_proofs.tex");
+  std::ofstream tex_examples("calculus_notes_examples.tex");
+  std::ofstream tex_def("calculus_notes_definitions.tex");
+  std::ifstream tex_source(file);
 
-	if (proofs && !tex_proofs.is_open()) {
-		proofs = false;
-		std::cout << "Unable to open proofs.tex" << std::endl;
-	}
-	if (examples && !tex_examples.is_open()) {
-		examples = false;
-		std::cout << "Unable to open examples.tex" << std::endl;
-	}
-	if (def && !tex_def.is_open()) {
-		def = false;
-		std::cout << "Unable to open def.tex" << std::endl;
-	}
+  if (proofs && !tex_proofs.is_open()) {
+    proofs = false;
+    std::cout << "Unable to open proofs.tex" << std::endl;
+  }
+  if (examples && !tex_examples.is_open()) {
+    examples = false;
+    std::cout << "Unable to open examples.tex" << std::endl;
+  }
+  if (def && !tex_def.is_open()) {
+    def = false;
+    std::cout << "Unable to open def.tex" << std::endl;
+  }
 
-	if(tex_source.is_open()) {
-		int read = BLOCK;
-		std::string line;
-		while (std::getline(tex_source,line)) {
-			if (line.find(BEGIN_BLOCK) != -1) {
-				read = BLOCK;
-			}
-			else if (line.find(BEGIN_PROOF) != -1) {
-				read = PROOF;
-			}
-			else if (line.find(BEGIN_EXAMPLE) != -1) {
-				read = EXAMPLE;
-			}
+  if(tex_source.is_open()) {
+    int read = BLOCK;
+    std::string line;
+    while (std::getline(tex_source,line)) {
+      if (line.find(BEGIN_BLOCK) != -1) {
+        read = BLOCK;
+      }
+      else if (line.find(BEGIN_PROOF) != -1) {
+        read = PROOF;
+      }
+      else if (line.find(BEGIN_EXAMPLE) != -1) {
+        read = EXAMPLE;
+      }
 
-			if (def && read == BLOCK) {
-				tex_def << line << std::endl;
-			}
-			if (proofs && read != EXAMPLE) {
-				tex_proofs << line << std::endl;
-			}
-			if (examples && read != PROOF) {
-				tex_examples << line << std::endl;
-			}
+      if (def && read == BLOCK) {
+        tex_def << line << std::endl;
+      }
+      if (proofs && read != EXAMPLE) {
+        tex_proofs << line << std::endl;
+      }
+      if (examples && read != PROOF) {
+        tex_examples << line << std::endl;
+      }
 
-			if (line.find(END_PROOF) != -1 || line.find(END_EXAMPLE) != -1) {
-				read = BLOCK;
-			}
-		}
-		tex_proofs.close();
-		tex_examples.close();
-		tex_def.close();
-		tex_source.close();
-	}
-	else {
-		std::cout << "Unable to open " << file << std::endl;
-	}
+      if (line.find(END_PROOF) != -1 || line.find(END_EXAMPLE) != -1) {
+        read = BLOCK;
+      }
+    }
+    tex_proofs.close();
+    tex_examples.close();
+    tex_def.close();
+    tex_source.close();
+  }
+  else {
+    std::cout << "Unable to open " << file << std::endl;
+  }
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-	generate_tex("calculus_notes_complete.tex", true, true, true);
-	return 0;
+  std::string path;
+  for(int i = 1; i < argc; i++) {
+    path += argv[i];
+  }
+  generate_tex(path, true, true, true);
+  return 0;
 }
